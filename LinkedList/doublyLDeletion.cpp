bf71cd8e68ed5e7ffd16bdba1a7f9b54af99efd1
Anuj sharma <anujsharma1624007@gmail.com>
2026-09-16 16:32:50 +0530

    doublyLinklist Deletion Algorithim

---
+++ LinkedList/doublyLDeletion.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+    Node *prev;   //stores the previous data 
+};
+
+int main(){
+    Node *head = new Node();
+    Node *A = new Node();
+    Node *B = new Node();
+    Node *C = new Node();
+    Node *D = new Node();
+
+    head->data = 0; //head node data is 0
+    A->data = 10;
+    B->data = 20;
+    C->data = 30;
+    D->data = 40;
+
+    head->next = A;
+    head->prev = NULL;
+    
+    A->next = B;
+    A->prev = head;
+
+    B->next = C;
+    B->prev = A;
+
+    C->next = D;
+    C->prev = B;
+
+    D->next = NULL;
+    D->prev = C;
+
+
+    //print the doubly linked list forward
+    cout<<"Doubly Linked list forward : ";
+    Node *temp = head;
+    while(temp!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+
+    //print the doubly linked list backward
+
+    temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+
+    cout<<"Doubly Linked list backward : ";
+    while(temp!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->prev;
+    }
+    cout<<endl;
+
+    //DELETION AT THE BEGINGING OF THE DOUBLY LINKLIST
+    Node *del = head;
+    head = head->next;
+    head->prev = NULL;
+    delete del;
+
+    temp = head;
+    cout<<"DELETION AT THE BEGINGING OF THE DOUBLY LINKLIST : ";
+    while(temp!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+
+
+    //DELETION AT THE ENDING OF THE DOUBLY LINKLIST
+    temp = head;
+    while(temp->next->next != NULL){
+        temp = temp ->next;
+    }
+
+    del = temp->next;
+    temp->next = NULL;
+    delete del;
+
+    temp = head;
+    cout<<"DELETION AT THE ENDING OF THE DOUBLY LINKLIST : ";
+    while(temp!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+
+    //DELETION AT THE PARTICULAR POSTION 3 IN THE DOUBLY LINKLIST
+    temp= head;
+    int pos = 2;
+    for(int i=1;i<pos-1 && temp!=NULL;i++){
+        temp = temp->next;
+    }
+    del = temp->next;
+    temp->next = del->next;
+    del->next->prev = temp;
+    delete del;
+
+    temp = head;
+    cout<<"DELETION AT THE PARTICULAR POSITION OF THE DOUBLY LINKLIST : ";
+    while(temp!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
